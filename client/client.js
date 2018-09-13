afda33f59e7d4ad2e77f10b96042c47b3e356dc6
RogerHerazo <rogerh@uninorte.edu.co>
2018-09-13 00:54:27 -0500

    Initial set up of the database page

---
+++ client/client.js
@@ -0,0 +1,7 @@
+console.log("Hello World!");
+const form  = document.querySelector("form");
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log("Form was Submited");
+});
