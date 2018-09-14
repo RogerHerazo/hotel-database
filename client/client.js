0e3ba5f33b5b270cd837caa179fce0c8f653b230
RogerHerazo <rogerh@uninorte.edu.co>
2018-09-13 23:50:50 -0500

    Populated Database from test file Hotels.json using a Python script

--- client/client.js
+++ client/client.js
@@ -1,7 +1,53 @@
 console.log("Hello World!");
-const form  = document.querySelector("form");
+const form  = document.querySelector('form');
+const API_URL = 'http://localhost:5000';
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log("Form was Submited");
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const address = formData.get('adress');
+    const state = formData.get('state');
+    const phone = formData.get('phone');
+    const fax = formData.get('fax');
+    const email_id = formData.get('email');
+    const website = formData.get('website');
+    const type = formData.get('type');
+    const rooms = formData.get('rooms');
+    
+    const hotel = {
+        name,
+        address,
+        state,
+        phone,
+        fax,
+        email_id,
+        website,
+        type,
+        rooms
+    };
+
+    fetch(API_URL+'/add', {
+        method: 'POST',
+        body: JSON.stringify(hotel),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(response => response.json())
+      .then(createdHotel => {
+          console.log(createdHotel);
+      });
 });
+
+const del = document.getElementById('delete');
+
+/*
+del.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log("Database Cleared");
+    fetch(API_URL+'/del', {
+        method: 'DELETE'
+    });
+});
+*/
+
--- server/index.js
+++ server/index.js
@@ -1,12 +1,94 @@
 const express = require('express');
+const cors = require('cors');
+const monk = require('monk');
+
 
 const app = express();
+const db = monk('localhost/hotelmadeeasy');
+const hotels = db.get('hotels');
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
+    hotels
+        .find()
+        .then(hotels => {
+            res.json(hotels);
+        });
+});
+
+app.get('/hotel-name', (req,res) => {
+
+});
+
+app.get('/state', (req,res) => {
+
+});
+
+app.get('/hotel-type', (req,res) => {
+
+});
+
+app.get('/size', (req,res) => {
+
+});
+
+function isValidHotel(hotel){
+    return hotel.name && hotel.name.toString().trim() !== '' &&
+           hotel.address && hotel.address.toString().trim() !== '' &&
+           hotel.state && hotel.state.toString().trim() !== '' &&
+           hotel.phone && hotel.phone.toString().trim() !== '' &&
+           hotel.fax && hotel.fax.toString().trim() !== '' &&
+           hotel.email_id && hotel.email_id.toString().trim() !== '' &&
+           hotel.website && hotel.website.toString().trim() !== '' &&
+           hotel.type && hotel.type.toString().trim() !== '' &&
+           hotel.rooms && hotel.rooms.toString().trim() !== '';
+}
+
+app.post('/add', (req,res) => {
+    console.log(req.body);
+    if(isValidHotel(req.body)){
+        const hotel = {
+            NAME: req.body.name.toString(),
+            ADDRESS: req.body.address.toString(),
+            STATE: req.body.state.toString(),
+            PHONE: req.body.phone.toString(),
+            FAX: req.body.fax.toString(),
+            EMAIL_ID: req.body.email_id.toString(),
+            WEBSITE: req.body.website.toString(),
+            TYPE: req.body.type.toString(),
+            ROOMS: req.body.rooms.toString()
+        };
+        console.log(hotel);
+        hotels
+            .insert(hotel)
+            .then(createdHotel => {
+                console.log(createdHotel);
+                res.json(createdHotel)
+            });
+
+    }else{
+        console.log('ERROR');
+        res.status(422);
+        res.json({
+            message: 'You did not complete all the fields'
+        });
+    }
+});
+
+app.put('/update', (req,res) => {
+
+});
+
+/*
+app.delete('/del', (req,res) =>{
+    hotels.remove({});
     res.json({
-        message: 'Hotel'
+        message: 'Deleted'
     });
 });
+*/
 
 app.listen(5000, () => {
     console.log('Listening on http://localhost:5000');
